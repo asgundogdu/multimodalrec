cf782e1abe636f69bbfeb1025df92bc25e6c88d7
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-11-18 21:18:01 -0500

    mobilenet

--- multimodalrec/image.py
+++ multimodalrec/image.py
@@ -1,5 +1,7 @@
 import os.path, re
 from keras.applications.inception_v3 import InceptionV3
+from keras.applications import mobilenet
+
 from keras.preprocessing import image
 from keras.applications.inception_v3 import preprocess_input
 from keras.models import Model, load_model
@@ -9,7 +11,7 @@ import numpy as np
 
 
 class Extractor():
-    def __init__(self, weights=None, load=False):
+    def __init__(self, weights=None, load=False, model_name='mobilenet'):
         """Either load pretrained from imagenet, or load our saved
         weights from own training."""
 
@@ -47,14 +49,53 @@ class Extractor():
         return features
 
 
-def extract_features(_dir_='',load=False):
+class ExtractorMobile():
+    def __init__(self, weights=None, load=False):
+        """Either load pretrained from imagenet, or load our saved
+        weights from own training."""
+
+        self.weights = weights  # so we can check elsewhere which model
+
+        if weights is None and not load:
+            # Get model with pretrained weights.
+            base_model = mobilenet.MobileNet(weights='imagenet')
+
+            # We'll extract features at the final pool layer.
+            self.model = Model(
+                inputs=base_model.input,
+                outputs=base_model.get_layer('avg_pool').output
+            )
+
+    def extract(self, image_path):
+        img = image.load_img(image_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+
+        # Get the prediction.
+        features = self.model.predict(x)
+
+        # if self.weights is None:
+        #     # For imagenet/default network:
+        #     features = features[0]
+        # else:
+        #     # For loaded network:
+        features = features[0]
+
+        return features
+
+
+def extract_features(_dir_='',load=False, model_name='mobilenet'):
     seq_lenght = 30
     train_dir = [_dir_+'train/'+f for f in os.listdir(_dir_+'train/') if f.find('.')==-1]
     test_dir = [_dir_+'test/'+f for f in os.listdir(_dir_+'test/') if f.find('.')==-1]
     # print(test_dir)
     all_data = train_dir+test_dir
 
-    model = Extractor(load=load)
+    if model_name=='mobilenet':
+        model = ExtractorMobile(load=load, model_name=model_name)
+    else:
+        model = Extractor(load=load, model_name=model_name)
 
     pbar = tqdm(total=len(all_data))
     all_sequences = {}
@@ -80,6 +121,9 @@ def extract_features(_dir_='',load=False):
     pbar.close()
 
     return all_sequences
+
+
+
 
 
 
