bec839b9afdd298c8e5b0e1abb95d00f6eec6ea3
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2019-01-12 21:13:34 -0500

    calc rep

---
+++ calc_image_representation.py
@@ -0,0 +1,12 @@
+from multimodalrec.multimodalrec import MultimodalRec
+from multimodalrec.multimodalrec import data_pipeline
+from multimodalrec.model import model
+
+def main():
+	recmodel = MultimodalRec()
+	recmodel.organize_multimodal_data(load=False, dataset=10, 
+		trailer_directory='/home/ubuntu/gits/multimodalrec/croped_frames/', 
+		sequence_dir = '/home/ubuntu/gits/multimodalrec')
+
+if __name__ == '__main__':
+	main()
